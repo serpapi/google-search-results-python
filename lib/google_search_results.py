e6b8085910c00d9e1c2ceccfeff32cffee4d6709
lf2225 <lenny.fishler@opensesame.com>
2018-01-14 13:05:02 -0500

    json and dicts fully working

--- lib/google_search_results.py
+++ lib/google_search_results.py
@@ -1,9 +1,10 @@
 import os
 import requests
+import json
 
 class GoogleSearchResults(object):
     VERSION = "1.0.0"
-    BACKEND = "http://serpapi.com/search"
+    BACKEND = "https://serpapi.com/search"
     SERP_API_KEY = None
 
     def __init__(self, params_dict):
@@ -11,7 +12,7 @@ class GoogleSearchResults(object):
 
     def construct_url(self):
         self.params_dict['source'] = 'python'
-        if GoogleSearchResults.SERP_API_KEY:
+        if self.SERP_API_KEY:
             self.params_dict['serp_api_key'] = self.SERP_API_KEY
         response = requests.get(self.BACKEND, self.params_dict)
         return response.text
@@ -28,14 +29,14 @@ class GoogleSearchResults(object):
 
     def get_json(self):
         self.params_dict["output"] = "json"
-        return self.get_results()
+        return json.loads(self.get_results())
 
     def get_json_with_images(self):
         self.params_dict["output"] = "json_with_images"
-        return self.get_results()
+        return json.loads(self.get_results())
 
     def get_dictionary(self):
-        return json.loads(dict(get_json))
+        return dict(self.get_json())
 
     def get_dictionary_with_images(self):
-        return json.loads(dict(get_json_with_images))
+        return dict(self.get_json_with_images())
