01d1b7753bfc1c152482730b7232790d9e13ae96
jvmvik <jvmvik@gmail.com>
2020-06-30 08:39:07 -0500

    follow-up latest python packaging guide for python 3+

---
+++ serpapi/__init__.py
@@ -0,0 +1,7 @@
+from .serp_api_client import SerpApiClient
+from .baidu_search_results import BaiduSearchResults
+from .google_search_results import GoogleSearchResults
+from .yahoo_search_results import YahooSearchResults
+from .bing_search_results import BingSearchResults
+from .yandex_search_results import YandexSearchResults
+from .google_scholar_search_results import GoogleScholarSearchResults
