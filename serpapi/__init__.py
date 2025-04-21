302c8ad9ddf403f5d4ad5ed86bc48a44206d7093
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-04-21 08:50:20 +0200

    :wrench: expose both sync and async interfaces in package top level import

--- serpapi/__init__.py
+++ serpapi/__init__.py
@@ -1,3 +1,4 @@
+# synchronous part
 from .serp_api_client import SerpApiClient
 from .baidu_search import BaiduSearch
 from .google_search import GoogleSearch
@@ -12,4 +13,50 @@ from .duck_duck_go_search import DuckDuckGoSearch
 from .walmart_search import WalmartSearch
 from .naver_search import NaverSearch
 from .apple_app_store_search import AppleAppStoreSearch
+# asynchronous mirror counter part
+from ._async.serp_api_client import AsyncSerpApiClient
+from ._async.baidu_search import AsyncBaiduSearch
+from ._async.google_search import AsyncGoogleSearch
+from ._async.yahoo_search import AsyncYahooSearch
+from ._async.bing_search import AsyncBingSearch
+from ._async.yandex_search import AsyncYandexSearch
+from ._async.google_scholar_search import AsyncGoogleScholarSearch
+from ._async.ebay_search import AsyncEbaySearch
+from ._async.home_depot_search import AsyncHomeDepotSearch
+from ._async.youtube_search import AsyncYoutubeSearch
+from ._async.duck_duck_go_search import AsyncDuckDuckGoSearch
+from ._async.walmart_search import AsyncWalmartSearch
+from ._async.naver_search import AsyncNaverSearch
+from ._async.apple_app_store_search import AsyncAppleAppStoreSearch
 
+
+__all__ = (
+    "SerpApiClient",
+    "BaiduSearch",
+    "GoogleSearch",
+    "YahooSearch",
+    "BingSearch",
+    "YandexSearch",
+    "GoogleScholarSearch",
+    "EbaySearch",
+    "HomeDepotSearch",
+    "YoutubeSearch",
+    "DuckDuckGoSearch",
+    "WalmartSearch",
+    "NaverSearch",
+    "AppleAppStoreSearch",
+    "AsyncSerpApiClient",
+    "AsyncBaiduSearch",
+    "AsyncGoogleSearch",
+    "AsyncYahooSearch",
+    "AsyncBingSearch",
+    "AsyncYandexSearch",
+    "AsyncGoogleScholarSearch",
+    "AsyncEbaySearch",
+    "AsyncHomeDepotSearch",
+    "AsyncYoutubeSearch",
+    "AsyncDuckDuckGoSearch",
+    "AsyncWalmartSearch",
+    "AsyncNaverSearch",
+    "AsyncAppleAppStoreSearch",
+)
